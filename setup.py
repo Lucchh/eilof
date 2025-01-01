162e388f580124d849fa3792e6121e0351ec2236
Lucchh <lucchenn@gmail.com>
2024-12-31 21:45:19 -0500

    General updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="eilof",  
-    version="1.0.3",  
+    version="1.0.4",  
     description="Efficient implementation of Incremental Local Outlier Factor (EILOF).",
     long_description=long_description,  # Link README.md content
     long_description_content_type="text/markdown",  # Specify Markdown format for PyPI
@@ -15,11 +15,11 @@ setup(
     url="https://github.com/Lucchh/eilof",  
     packages=find_packages(), 
     install_requires=[
-        "numpy>=1.20.0,<3.0.0",         
-        "scipy>=1.13.0,<2.0.0",       
-        "matplotlib>=3.9.0,<4.0.0",   
-        "seaborn>=0.13.0,<1.0.0",     
-        "scikit-learn>=1.6.0,<2.0.0", 
+        "numpy>=1.19.5,<2.0.0",         
+        "scipy>=1.6.0,<2.0.0",       
+        "matplotlib>=3.3.4,<4.0.0",   
+        "seaborn>=0.9.0,<1.0.0",    
+        "scikit-learn>=1.0.0,<2.0.0",
     ],
     python_requires=">=3.8",  
     classifiers=[
